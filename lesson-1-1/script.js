32f5e01eb983c208f8a4bc0c5725f6cb09b495f5
Oleg <meduzka321@gmail.com>
2018-10-12 21:35:30 +0300

    Еще правки по первому дз доп задание

--- lesson-1-1/script.js
+++ lesson-1-1/script.js
@@ -2,13 +2,13 @@
 
 var num = prompt("Введите 5-ти значное число", "33721");
 var string = num;
-var multiply = +string.charAt(0) * string.charAt(1) * string.charAt(2) * string.charAt(3) * string.charAt(4);
+var multiply = string.charAt(0) * string.charAt(1) * string.charAt(2) * string.charAt(3) * string.charAt(4);
 
 console.log(multiply);
 
 var cube = multiply ** 3;
 var firstTwoNum = String(cube);
 
-alert(+firstTwoNum.charAt(0) + firstTwoNum.charAt(1));
+alert(firstTwoNum.charAt(0) + firstTwoNum.charAt(1));
 
 
